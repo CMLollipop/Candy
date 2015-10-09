7c832b2a346423c8a2902c9c166a227a06b98918
caiming <mcai@91jkys.com>
2015-10-09 14:48:02 +0800

    create project

---
+++ Candy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Candy
+//
+//  Created by caiming on 15/10/9.
+//  Copyright © 2015年 caiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
